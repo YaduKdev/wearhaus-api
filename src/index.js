6b4ed42d65cedd5a74fd35f406f7fe2c4063cd36
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2024-12-15 03:59:50 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// require("specifier-resolution-node").register();
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+import app from ".";
+
+const PORT = 8200;
+
+app.listen(PORT, () => console.log(`WEARHAUS app is running on port ${PORT}`));
