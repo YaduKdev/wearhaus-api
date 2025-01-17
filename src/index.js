5fd9ba4206251d577f5be6949cfcee942952e68d
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-01-17 14:38:36 +0530

    add vercel cors origin

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:4200", "https://wearhaus-view.onrender.com"],
+  origin: [
+    "http://localhost:4200",
+    "https://wearhaus-view.onrender.com",
+    "https://wearhaus.vercel.app/",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
